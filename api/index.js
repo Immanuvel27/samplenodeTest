532120340d48f94915875b194d5708dca6851293
MIMMANUVEL <mimmanuvel@genixtec.com>
2026-03-03 12:54:45 +0530

    new thing

--- api/index.js
+++ api/index.js
@@ -1,4 +1,17 @@
-const serverless = require("serverless-http");
-const app = require("../server");
+const express = require('express');
+const cors = require('cors');
 
-module.exports = serverless(app);
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.json({ message: 'OK from Vercel 🚀' });
+});
+
+app.get('/api/testApi', (req, res) => {
+  res.json({ message: 'Test API working' });
+});
+
+module.exports = app;
